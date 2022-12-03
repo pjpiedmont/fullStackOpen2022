393a23b1af4ae53fd7ff4ca6cb1d8fb77a434999
Parker Piedmont <pjpiedmont@gmail.com>
2022-12-02 23:06:33 -0800

    Completed Exercise 1.11 (5 min)

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,7 +2,14 @@ import { useState } from 'react'
 
 const Button = ({ text, clickHandler }) => <button onClick={clickHandler}>{text}</button>
 
-const StatisticLine = ({ text, value }) => <p>{text} {value}</p>
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
 
 const Statistics = ({ good, neutral, bad }) => {
   const numResponses = good + neutral + bad
@@ -22,12 +29,16 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <h1>statistics</h1>
-      <StatisticLine text='good' value={good} />
-      <StatisticLine text='neutral' value={neutral} />
-      <StatisticLine text='bad' value={bad} />
-      <StatisticLine text='all' value={numResponses} />
-      <StatisticLine text='average' value={avg} />
-      <StatisticLine text='positive' value={positive} />
+      <table>
+        <tbody>
+          <StatisticLine text='good' value={good} />
+          <StatisticLine text='neutral' value={neutral} />
+          <StatisticLine text='bad' value={bad} />
+          <StatisticLine text='all' value={numResponses} />
+          <StatisticLine text='average' value={avg} />
+          <StatisticLine text='positive' value={positive} />
+        </tbody>
+      </table>
     </div>
   )
 }
