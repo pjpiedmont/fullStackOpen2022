02c3a35813c91989bdfd901be5848099a470f1ec
Parker Piedmont <pjpiedmont@gmail.com>
2023-01-07 15:50:03 -0800

    Completed Exercise 2.6 (28 min)

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 1 }]);
+  const [newName, setNewName] = useState("");
+
+  const addName = (event) => {
+    event.preventDefault();
+    const newNameObject = {
+      name: newName,
+      id: persons.length + 1,
+    };
+    setPersons(persons.concat(newNameObject));
+    setNewName("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => {
+        return <p key={person.id}>{person.name}</p>;
+      })}
+    </div>
+  );
+};
+
+export default App;
