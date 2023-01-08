a645f1a9f46d3696bb4fd3f24c2d629d9caff9c2
Parker Piedmont <pjpiedmont@gmail.com>
2023-01-07 19:09:14 -0800

    Completed Exercise 2.11 (5 min)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,60 +1,63 @@
-import { useState } from "react";
-import Filter from "./components/Filter";
-import PersonForm from "./components/PersonForm";
-import Persons from "./components/Persons";
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
-
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [filter, setFilter] = useState("");
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const personsToShow =
-    filter === ""
+    filter === ''
       ? persons
       : persons.filter((person) =>
-          person.name.toLowerCase().includes(filter.toLowerCase())
-        );
+        person.name.toLowerCase().includes(filter.toLowerCase())
+      )
 
   const addName = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
     if (persons.find((person) => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`);
+      alert(`${newName} is already added to phonebook`)
     } else {
       const newNameObject = {
         name: newName,
         number: newNumber,
         id: persons.length + 1,
-      };
-      setPersons(persons.concat(newNameObject));
+      }
+      setPersons(persons.concat(newNameObject))
     }
 
-    setNewName("");
-    setNewNumber("");
-  };
+    setNewName('')
+    setNewNumber('')
+  }
 
   const handleFormChange = (event) => {
-    if (event.target.name === "name") {
-      setNewName(event.target.value);
-      return;
+    if (event.target.name === 'name') {
+      setNewName(event.target.value)
+      return
     }
 
-    if (event.target.name === "number") {
-      setNewNumber(event.target.value);
-      return;
+    if (event.target.name === 'number') {
+      setNewNumber(event.target.value)
+      return
     }
-  };
+  }
 
   const handleFilterChange = (event) => {
-    setFilter(event.target.value);
-  };
+    setFilter(event.target.value)
+  }
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response) => {
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
@@ -72,7 +75,7 @@ const App = () => {
       <h2>Numbers</h2>
       <Persons persons={personsToShow} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
