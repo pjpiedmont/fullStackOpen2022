e86c122f240f1bf318b7c75669680299faafce55
Parker Piedmont <pjpiedmont@gmail.com>
2024-10-23 18:21:39 -0700

    Completed Exercise 2.15 (20 minutes)

--- part2/phonebook/src/services/person.js
+++ part2/phonebook/src/services/person.js
@@ -11,8 +11,13 @@ const add = (newPerson) => {
     return req.then(res => res.data)
 }
 
+const update = (id, newPerson) => {
+    const req = axios.put(`${baseUrl}/${id}`, newPerson)
+    return req.then(res => res.data)
+}
+
 const remove = (id) => {
     return axios.delete(`${baseUrl}/${id}`)
 }
 
-export default { getAll, add, remove }
+export default { getAll, add, update, remove }
