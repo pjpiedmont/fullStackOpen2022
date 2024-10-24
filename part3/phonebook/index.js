490b5c692c9461df34956c97c27e1c7ef92988bb
Parker Piedmont <pjpiedmont@gmail.com>
2024-10-24 00:51:01 -0700

    Completed Exercise 3.2 (10 minutes)

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -30,6 +30,15 @@ app.get('/api/persons', (req, res) => {
 	res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+	console.log(req)
+	res_text = `
+		<p>Phonebook has info for ${persons.length} people</p>
+		<p>${new Date()}</p>
+	`
+	res.send(res_text)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
