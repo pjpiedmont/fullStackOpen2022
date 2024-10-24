44566b7d5e13cb6641fd71a9d5d48fc8feb89cb1
Parker Piedmont <pjpiedmont@proton.me>
2024-10-24 00:55:38 -0700

    Completed Exercise 3.3 (10 minutes)

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,22 +3,22 @@ const app = express()
 
 let persons = [
 	{ 
-		"id": "1",
+		"id": 1,
 		"name": "Arto Hellas", 
 		"number": "040-123456"
 	},
 	{ 
-		"id": "2",
+		"id": 2,
 		"name": "Ada Lovelace", 
 		"number": "39-44-5323523"
 	},
 	{ 
-		"id": "3",
+		"id": 3,
 		"name": "Dan Abramov", 
 		"number": "12-43-234345"
 	},
 	{ 
-		"id": "4",
+		"id": 4,
 		"name": "Mary Poppendieck", 
 		"number": "39-23-6423122"
 	}
@@ -28,6 +28,22 @@ app.use(express.json())
 
 app.get('/api/persons', (req, res) => {
 	res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	const person = persons.find(p => p.id === id)
+
+	console.log(id, person)
+
+	if (person)
+	{
+		res.json(person)
+	}
+	else
+	{
+		res.status(404).end()
+	}
 })
 
 app.get('/info', (req, res) => {
