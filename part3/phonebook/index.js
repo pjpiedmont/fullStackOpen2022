f76607ecb1afdd285119ec12e397cd8436ba41e6
Parker Piedmont <pjpiedmont@proton.me>
2024-10-24 00:57:32 -0700

    Completed Exercise 3.4 (2 minutes)

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -46,6 +46,12 @@ app.get('/api/persons/:id', (req, res) => {
 	}
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	persons = persons.filter(p => p.id !== id)
+	res.status(204).end()
+})
+
 app.get('/info', (req, res) => {
 	console.log(req)
 	res_text = `
