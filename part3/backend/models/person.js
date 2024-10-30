8d4e5321e9d3d9734a7af2e65f47b63749e23796
Parker Piedmont <pjpiedmont@gmail.com>
2024-10-30 15:31:11 -0700

    Completed Exercise 3.20 (20 minutes)

--- part3/backend/models/person.js
+++ part3/backend/models/person.js
@@ -18,11 +18,18 @@ const personSchema = new mongoose.Schema({
     name: {
         type: String,
         minLength: 3,
-        required: true
+        required: [true, 'Person name required']
     },
     number: {
         type: String,
-        required: true
+        minLength: 8,
+        required: [true, 'Person phone number required'],
+        validate: {
+            validator: (v) => {
+                return /^\d{2,3}-\d+$/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number`
+        }
     }
 })
 
