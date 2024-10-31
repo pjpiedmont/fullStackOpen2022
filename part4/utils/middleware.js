2373008835742f9a4a9dfc8cd071b8415aa651ac
Parker Piedmont <pjpiedmont@gmail.com>
2024-10-31 13:02:22 -0700

    Completed Exercises 4.1 and 4.2 (30 minutes)

---
+++ part4/utils/middleware.js
@@ -0,0 +1,33 @@
+const logger = require('./logger')
+
+const requestLogger = (req, res, next) => {
+  logger.info('Method:', req.method)
+  logger.info('Path:  ', req.path)
+  logger.info('Body:  ', req.body)
+  logger.info('---')
+  next()
+}
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'endpoint does not exist' })
+}
+
+const errorHandler = (err, req, res, next) => {
+  logger.error(err.message)
+
+  if (err.name === 'CastERror') {
+    return res.status(400).send({ error: 'malformed ID' })
+  }
+
+  if (err.name === 'ValidationError') {
+    return res.status(400).json({ error: err.message })
+  }
+
+  next(err)
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
+}
