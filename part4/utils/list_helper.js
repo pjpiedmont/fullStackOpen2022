0f8566fd10edacba300d7716bee4ecfedc5df821
Parker Piedmont <pjpiedmont@gmail.com>
2024-11-01 14:06:08 -0700

    Completed Exercise 4.5 (15 minutes)

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -8,7 +8,22 @@ const totalLikes = (blogs) => {
   }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  return blogs.reduce((favorite, current) => {
+    if (current.likes > favorite.likes) {
+      favorite = current
+    }
+
+    return favorite
+  }, blogs[0])
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
