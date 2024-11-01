1add7c77b271b197eaaa711a4eac752252a02674
Parker Piedmont <pjpiedmont@gmail.com>
2024-11-01 13:54:37 -0700

    Completed Exercise 4.4 (10 minutes)

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, item) => {
+    return sum + item.likes
+  }, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
